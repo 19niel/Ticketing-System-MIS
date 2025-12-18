a9e46f1663a5c945a6b5a3e8bfe1af8b2cb47cdf
Nice Nice <kungfayla@gmail.com>
2025-12-18 16:34:34 +0800

    going back to login page after going to dashboard fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,53 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage"
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import TechSupportDashboard from "./pages/techsupport/TechSupportDashboard";
 import EmployeeDashboard from "./pages/employee/EmployeeDashboard";
 import ProtectedRoute from "./utils/ProtectedRoute";
+import PublicRoute from "./utils/PublicRoute";
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
+    <Routes>
+      {/* Login Page - Public Route */}
+      <Route
+        path="/"
+        element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
 
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Admin Dashboard */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/techsupport"
-          element={
-            <ProtectedRoute>
-              <TechSupportDashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Tech Support Dashboard */}
+      <Route
+        path="/techsupport"
+        element={
+          <ProtectedRoute>
+            <TechSupportDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/employee"
-          element={
-            <ProtectedRoute>
-              <EmployeeDashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      {/* Employee Dashboard */}
+      <Route
+        path="/employee"
+        element={
+          <ProtectedRoute>
+            <EmployeeDashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -1,26 +1,12 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  
-    const user = null 
-    return user ? <Outlet/> : <Navigate to="/"/>
-  
+export default function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    // User is not logged in
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 }
-
-export default ProtectedRoute
-
-
-
-// import { Navigate } from "react-router-dom";
-
-// export default function ProtectedRoute({ children }) {
-//   const token = localStorage.getItem("token");
-//   const roleId = localStorage.getItem("role_id");
-
-//   // 🔍 TEST LOGS for getting the token and the role ID
-//   // console.log("JWT Token:", token);
-//   console.log("Role ID:", roleId);
-
-//   // Temporary: allow access even if missing (for testing)  
-//   return children;
-// }
