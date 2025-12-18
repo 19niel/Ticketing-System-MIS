2ecfabb239a716938e806208a625da59dbaf94be
Nice Nice <kungfayla@gmail.com>
2025-12-18 16:11:24 +0800

    simple ProtectedRoute logic

--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -1,13 +1,26 @@
-import { Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
-  const roleId = localStorage.getItem("role_id");
-
-  // 🔍 TEST LOGS for getting the token and the role ID
-  // console.log("JWT Token:", token);
-  console.log("Role ID:", roleId);
-
-  // Temporary: allow access even if missing (for testing)
-  return children;
+const ProtectedRoute = () => {
+  
+    const user = null 
+    return user ? <Outlet/> : <Navigate to="/"/>
+  
 }
+
+export default ProtectedRoute
+
+
+
+// import { Navigate } from "react-router-dom";
+
+// export default function ProtectedRoute({ children }) {
+//   const token = localStorage.getItem("token");
+//   const roleId = localStorage.getItem("role_id");
+
+//   // 🔍 TEST LOGS for getting the token and the role ID
+//   // console.log("JWT Token:", token);
+//   console.log("Role ID:", roleId);
+
+//   // Temporary: allow access even if missing (for testing)  
+//   return children;
+// }
