ad08f07da7045fc413fe10905f4958cb9c742bc6
Nice Nice <kungfayla@gmail.com>
2025-12-26 17:20:31 +0800

    the roles and departments are now pre selected in the EditUserForm.jsx

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,21 +3,22 @@ import {db} from "../db.js";
 export const getAllUsers = async (req, res) => {
   try {
     const [rows] = await db.query(`
-        SELECT
+      SELECT
         u.user_id AS id,
         u.employee_id,
         CONCAT(u.first_name, ' ', u.last_name) AS name,
         u.email,
         u.position,
+        u.department_id,           -- Add this
         d.department_name AS department,
+        u.role_id,                 -- Add this
         r.role_name AS role,
         u.is_active
-        
-        FROM users u
-        JOIN departments d ON u.department_id = d.department_id
-        JOIN roles r ON u.role_id = r.role_id
-  ` 
-        );
+      FROM users u
+      JOIN departments d ON u.department_id = d.department_id
+      JOIN roles r ON u.role_id = r.role_id
+    `);
+
     res.status(200).json(rows);
   } catch (error) {
     console.error("Error fetching users:", error);
